4be011de9c70c5c5985901047749bbdd8fbd440b
KartashovSergey <sergey.po4ta.86@gmail.com>
2019-12-07 21:53:33 +0300

    added product-slider and product

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,22 @@
 $(function(){
 
+    
+
+    $('.product-slider__inner').slick({
+        dots:true,
+        arrows: false,
+        slidesToShow: 4,
+        slidesToScroll: 4
+
+    })
+
+    $(".rate-star").rateYo({
+        rating: 5,
+        starWidth: "12px",
+        readOnly: true
+    });
+    
+    var mixer = mixitup('.products__inner-boxs');
 
     
 })
